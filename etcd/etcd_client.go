3eb98045ad65f3620620ce16775e9aca125bf2f3
Lico-Tom <95178212+Lico-Tom@users.noreply.github.com>
2022-11-29 21:39:35 +0800

    support to export and import etcd content. (#7)

--- etcd/etcd_client.go
+++ etcd/etcd_client.go
@@ -19,13 +19,50 @@ package etcd
 
 import (
 	"context"
+	"github.com/sirupsen/logrus"
+	"go.etcd.io/etcd/api/v3/mvccpb"
+	v3 "go.etcd.io/etcd/client/v3"
 	"time"
 )
 
-func (h *Handler) GetKeyContent(ctx context.Context, key string) ([]byte, error) {
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+func (h *Handler) PutKey(ctx context.Context, key string, val string) error {
+	_, err := h.client.Put(ctx, key, val)
+	if err != nil {
+		logrus.Errorf("Error put key. %s", err)
+		return err
+	}
+	return nil
+}
+
+func (h *Handler) DeleteKey(ctx context.Context, key string) error {
+	_, err := h.client.Delete(ctx, key)
+	if err != nil {
+		logrus.Errorf("Error delete key. %s", err)
+		return err
+	}
+	return nil
+}
+
+func (h *Handler) KeysList(ctx context.Context) []string {
+	logrus.Info("begin to list keys")
+	clientCtx, cancel := context.WithTimeout(ctx, 15*time.Second)
 	defer cancel()
-	resp, err := h.client.Get(ctx, key)
+	getResponse, err := h.client.Get(clientCtx, "", v3.WithPrefix(), v3.WithKeysOnly(), v3.WithSerializable())
+	if err != nil {
+		logrus.Errorf("Error list keys. %s", err)
+		return nil
+	}
+	keys := make([]string, getResponse.Count)
+	for i, kv := range getResponse.Kvs {
+		keys[i] = string(kv.Key)
+	}
+	return keys
+}
+
+func (h *Handler) GetKeyContent(ctx context.Context, key string) ([]byte, error) {
+	clientCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	resp, err := h.client.Get(clientCtx, key)
 	if err != nil {
 		return nil, err
 	}
@@ -33,4 +70,17 @@ func (h *Handler) GetKeyContent(ctx context.Context, key string) ([]byte, error)
 		return nil, nil
 	}
 	return resp.Kvs[0].Value, nil
+}
+
+func (h *Handler) GetAllKeyContent(ctx context.Context) ([]*mvccpb.KeyValue, error) {
+	clientCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	resp, err := h.client.Get(clientCtx, "", v3.WithPrefix(), v3.WithSerializable())
+	if err != nil {
+		return nil, err
+	}
+	if len(resp.Kvs) == 0 {
+		return nil, nil
+	}
+	return resp.Kvs, nil
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/cobra v1.6.1
+	go.etcd.io/etcd/api/v3 v3.5.6
 	go.etcd.io/etcd/client/v3 v3.5.6
 	k8s.io/kubectl v0.25.4
 )
@@ -22,8 +23,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/testify v1.8.0 // indirect
-	go.etcd.io/etcd/api/v3 v3.5.6 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.6 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
